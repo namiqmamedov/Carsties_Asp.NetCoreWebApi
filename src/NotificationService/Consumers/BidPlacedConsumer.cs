9b7a38b782ae440c4e0d00eba4a5c4620fe8d702
namiqmamedov <namigmm@code.edu.az>
2023-12-12 18:17:25 +0400

    Adding a SignalR service

---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("==> bid placed message received");
+
+            await _hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+        }
+    }
+}
---
+++ src/NotificationService/Hubs/NotificationHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        
+    }
+}
